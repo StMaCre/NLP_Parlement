b43981a3cfb44c8f235ad2d94a02c26d11d60848
StMaCre <stephanie.creteur@gmail.com>
2023-05-30 16:33:35 +0300

    Creation of the connection to drive file
    Connection from the Google drive to the program

---
+++ connection_drive.py
@@ -0,0 +1,71 @@
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+import os
+import pickle
+import io
+
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+folder_id = '1uDrwv__C_4VqDbqe1UAd5iZAX3UFwrHI'
+
+
+def download_file(service, file_id, filename, download_path):
+    request = service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+
+    fh.seek(0)
+
+    with open(os.path.join(download_path, filename), 'wb') as f:
+        f.write(fh.read())
+        f.close()
+
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                r'C:\Users\Stephanie\Documents\GitHub\NLP_Parlement\client_secret_999205551216-6256q9bovmemq7qbmigrrt2828s6en3p.apps.googleusercontent.com.json', SCOPES)
+            creds = flow.run_local_server(port=8080)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    results = service.files().list(
+        q="'" + folder_id + "' in parents",
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+            filename = item['name']
+            download_file(service, item['id'], filename, 'pdf')
+
+    # Now all the files have been downloaded, you can read them one by one
+    for item in items:
+        filename = item['name']
+        with open(filename, 'r') as f:
+            content = f.read()
+        print(content)
+
+
+if __name__ == '__main__':
+    main()
